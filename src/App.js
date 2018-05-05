200a9335e2ecd7f68c5e3adfe4e2a3e5b283a762
Ryan Butler <b6utle5r@gmail.com>
2018-05-05 13:54:35 -0600

    Hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
 import Dashboard from './Components/Dashboard/Dashboard';
 import UpdateUser from './Components/UpdateUser/UpdateUser';
 import AddPark from './Components/AddPark/AddPark';
@@ -14,12 +14,12 @@ class App extends Component {
       <div className="App">
       <Router>
         <Switch>
-        <Route path={`/dashboard`} component={Dashboard}/>
-        <Route path={`/new-user`} component={Register}/>
-        <Route path={`/updateuser`} component={UpdateUser}/>
-        <Route path={`/addpark`} component={AddPark}/>
-        <Route path={`/details/:id`} component={Details}/>
-        <Route path={`/`} component={Login}/>
+        <Route path={`#/dashboard`} component={Dashboard}/>
+        <Route path={`#/new-user`} component={Register}/>
+        <Route path={`#/updateuser`} component={UpdateUser}/>
+        <Route path={`#/addpark`} component={AddPark}/>
+        <Route path={`#/details/:id`} component={Details}/>
+        <Route path={`#/`} component={Login}/>
         </Switch>
 
       </Router>
