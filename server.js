98468f7b77d4346fc73f99ac7de5d73a59fb5367
Ryan Butler <b6utle5r@gmail.com>
2018-05-05 12:45:55 -0600

    tweaks

--- server.js
+++ server.js
@@ -1,259 +1,282 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const massive = require('massive');
-const session = require('express-session');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const massive = require("massive");
+const session = require("express-session");
+const path = require("path");
 // const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
-require('dotenv').config({ path: __dirname + '/.env'});
+require("dotenv").config({ path: __dirname + "/.env" });
 
-const {getPark, parkDetail, parkCampgrounds} = require('./server/NPService');
-const {getMap} = require('./server/MapService');
+const { getPark, parkDetail, parkCampgrounds } = require("./server/NPService");
+const { getMap } = require("./server/MapService");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static('./build'));
 
 massive(process.env.CONNECTION_STRING)
-    .then((db)=>{
-        console.log('db is connected');
-        app.set('db', db);
+  .then(db => {
+    console.log("db is connected");
+    app.set("db", db);
+  })
+  .catch(err => {
+    console.warn("Failed to connect:");
+    console.error(err);
+  });
+app.use(checkDb());
+
+app.use(
+  session({
+    name: "MyPark",
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+      expires: 5 * 24 * 60 * 60 * 1000
+    },
+    saveUninitialized: false,
+    rolling: true,
+    resave: false
+  })
+);
+
+app.get("/api/hello", (req, res) => {
+  res.send("This worked");
+});
+
+app.get("/api/parks", (req, res) => {
+  getPark(req.query.state).then(r => {
+    res.send(r).status(200);
+  });
+});
+app.get("/api/park-details/:id", (req, res) => {
+  parkDetail(req.params.id)
+    .then(r => {
+      res.send(r).status(200);
     })
     .catch(err => {
-        console.warn('Failed to connect:');
-        console.error(err);
+      throw err;
     });
-    app.use(checkDb());
-
-    app.use(session({
-        name: 'MyPark',
-        secret: process.env.SESSION_SECRET,
-        cookie: {
-            expires:  5 * 24 * 60 * 60 *1000,
-        },
-        saveUninitialized: false,
-        rolling: true,
-        resave: false,
-    }));
-
-
-
-app.get('/api/hello', (req, res)=>{
-    res.send('This worked');
-})
-
-app.get('/api/parks',(req,res) => {
-    getPark(req.query.state)
-        .then(r => {
-        res.send(r).status(200);
+});
+app.post("/api/park-map", (req, res) => {
+  const destination = req.body.destination;
+  const origin = req.body.origin;
+  getMap(destination, origin)
+    .then(r => {
+      res.send(r);
     })
+    .catch(err => {
+      throw err;
     });
-app.get('/api/park-details/:id',(req,res) => {
-        parkDetail(req.params.id)
-          .then(r => {
-            res.send(r).status(200);
-        })
-        .catch(err=>{
-            throw err;
-        })
-    });
-app.post('/api/park-map',(req,res) => {
-    const destination = req.body.destination;
-    const origin = req.body.origin;
-    getMap(destination, origin)
-       .then(r=>{
-           res.send(r)
-       })
-       .catch(err=>{
-           throw err;
-       })
-    });
+});
 
-app.get(`/api/campgrounds/:id`, (req,res)=>{
-    parkCampgrounds(req.params.id)
-        .then(campgrounds => {
-            res.send(campgrounds).status(200)
-        })
-        .catch(err =>{
-            throw err;
-        })
+app.get(`/api/campgrounds/:id`, (req, res) => {
+  parkCampgrounds(req.params.id)
+    .then(campgrounds => {
+      res.send(campgrounds).status(200);
     })
-
-app.post('/api/login', (req, res) => {
-      const { email, password } = req.body;
-
-      req.db.users.findOne({ email, password })
-          .then(user => {
-              if (!user) {
-                  return res.status(401).send({ success: false, message: 'it did not work' });
-              }
-              req.session.user = user.id
-              res.send({ success: true, message: 'Logged in successfully' });
-          })
-          .catch(err=>{
-            console.log("invalid credentials")
-          });
+    .catch(err => {
+      throw err;
     });
+});
 
-app.post('/api/register', (req, res) => {
-      const { email, password, firstName, lastName, city, state, zip } = req.body;
+app.post("/api/login", (req, res) => {
+  const { email, password } = req.body;
 
-     password = bcrypt.hashSync(password, bcrypt.genSaltSync(15));
-        req.db.users.insert({firstname:firstName, lastname:lastName, email, password,  city, state, zip })
-        .then(user => {
-        req.session.user = user.id
-            res.send({ success: true, message: 'logged in successfully' });
-        })
-          .catch(err =>{
-            throw err;
-            }
-        );
+  req.db.users
+    .findOne({ email, password })
+    .then(user => {
+      if (!user) {
+        return res
+          .status(401)
+          .send({ success: false, message: "it did not work" });
+      }
+      req.session.user = user.id;
+      res.send({ success: true, message: "Logged in successfully" });
+    })
+    .catch(err => {
+      console.log("invalid credentials");
     });
+});
 
-app.put(`/api/update-user`,(req,res) =>{
-    const { email, password, firstName, lastName, city, state, zip, id } = req.body;
+app.post("/api/register", (req, res) => {
+  const { email, password, firstName, lastName, city, state, zip } = req.body;
 
-    req.db.updateUser({firstName, lastName, email, password,  city, state, zip, id})
+  password = bcrypt.hashSync(password, bcrypt.genSaltSync(15));
+  req.db.users
+    .insert({
+      firstname: firstName,
+      lastname: lastName,
+      email,
+      password,
+      city,
+      state,
+      zip
+    })
+    .then(user => {
+      req.session.user = user.id;
+      res.send({ success: true, message: "logged in successfully" });
+    })
+    .catch(err => {
+      throw err;
+    });
+});
+
+app.put(`/api/update-user`, (req, res) => {
+  const {
+    email,
+    password,
+    firstName,
+    lastName,
+    city,
+    state,
+    zip,
+    id
+  } = req.body;
+
+  req.db
+    .updateUser({ firstName, lastName, email, password, city, state, zip, id })
     .then(updatedUser => {
-        res.send(updatedUser)
-    })
-    })
-app.get(`/api/logout`, (req, res) =>{
-        req.session.destroy();
-        res.send({ success: true, message: 'Logged out successfully' });
-    })
-app.get('/api/user-data', (req, res) => {
-        req.db.getUserInfo([req.session.user])
-        .then(r => {
-        res.send(r)
-        })
+      res.send(updatedUser);
     });
-app.post(`/api/park-data`, (req,res) =>{
-        const userid = req.session.user;
-        const parkid = req.body.parkid;
+});
+app.get(`/api/logout`, (req, res) => {
+  req.session.destroy();
+  res.send({ success: true, message: "Logged out successfully" });
+});
+app.get("/api/user-data", (req, res) => {
+  req.db.getUserInfo([req.session.user]).then(r => {
+    res.send(r);
+  });
+});
+app.post(`/api/park-data`, (req, res) => {
+  const userid = req.session.user;
+  const parkid = req.body.parkid;
 
-        req.db.user_parks.insert({parkid, userid})
-        .then((r)=>{
-            return  req.db.parks.find({parkid});
-        })
-        .then(r=>{
-            if(r.length <= 0){
-                return req.db.parks.insert({parkid})
-            }
-        })
-        .then(()=>{
-           return req.db.fetchParks({userid})
-            .then(parks =>{
-            res.send(parks)
-        })
-        .catch((err)=>{
-            throw err;
-        })
+  req.db.user_parks
+    .insert({ parkid, userid })
+    .then(r => {
+      return req.db.parks.find({ parkid });
     })
-})
+    .then(r => {
+      if (r.length <= 0) {
+        return req.db.parks.insert({ parkid });
+      }
+    })
+    .then(() => {
+      return req.db
+        .fetchParks({ userid })
+        .then(parks => {
+          res.send(parks);
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+});
 
-app.get(`/api/park-id-list`, (req,res)=>{
-    const userId = req.session.user;
-    return req.db.getParkId({userId})
-    .then(parkId=>{
-        res.send(parkId)
+app.get(`/api/park-id-list`, (req, res) => {
+  const userId = req.session.user;
+  return req.db.getParkId({ userId }).then(parkId => {
+    res.send(parkId);
+  });
+});
+app.get(`/api/myparks`, (req, res) => {
+  const userid = req.session.user;
+  req.db
+    .fetchParks({ userid })
+    .then(parks => {
+      const promises = parks.map(park => {
+        const parkId = park.parkid;
+        return parkDetail(parkId);
+      });
+      return Promise.all(promises);
     })
-})
-app.get(`/api/myparks`,(req,res)=>{
-    const userid = req.session.user;
-   req.db.fetchParks({userid})
-   .then(parks =>{
-    const promises = parks.map(park=>{
-      const parkId= park.parkid
-     return parkDetail(parkId)
+    .then(parksData => {
+      res.send(parksData);
+    });
+});
+app.get(`/api/get-comments/:id`, (req, res) => {
+  const parkid = req.params.id;
+  req.db
+    .getComments({ parkid })
+    .then(allComments => {
+      res.send(allComments);
     })
-   return Promise.all(promises)
-   })
-   .then(parksData =>{
-       res.send(parksData)
-   })
-    })
-app.get(`/api/get-comments/:id`, (req,res)=>{
-   const parkid = req.params.id
-    req.db.getComments({parkid})
-        .then(allComments =>{
-            res.send(allComments)
-        })
-        .catch(err=>{
-            throw err;
-        })
-    })
+    .catch(err => {
+      throw err;
+    });
+});
 
-app.post(`/api/add-comment`,(req,res)=>{
-    const userId = req.session.user;
-    const parkid = req.body.comments.parkCode;
-    const title = req.body.comments.commentTitle;
-    const description = req.body.comments.comments;
+app.post(`/api/add-comment`, (req, res) => {
+  const userId = req.session.user;
+  const parkid = req.body.comments.parkCode;
+  const title = req.body.comments.commentTitle;
+  const description = req.body.comments.comments;
 
-    req.db.addComment({userId, parkid, title, description})
-        .then((r)=>{
-           return req.db.reviews.find({parkid})
-        })
-        .then(data => {
-            res.send(data)
-
-        })
-        .catch((err)=>{
-            throw err;
-        })
-
+  req.db
+    .addComment({ userId, parkid, title, description })
+    .then(r => {
+      return req.db.reviews.find({ parkid });
     })
-app.delete(`/api/remove-comment/:id`, (req, res)=>{
-    req.db.deleteComment(req.params.id)
-    .then((r)=>{
-        const parkid = req.body.park
-        req.db.getComments({parkid})
-            .then(allComments =>{
-                res.send(allComments)
-            })
-            .catch(err=>{
-                throw err;
-            })
+    .then(data => {
+      res.send(data);
     })
-})
-app.delete(`/api/remove/:id`,(req,res)=>{
-    req.db.removePark({parkId: req.params.id, userId: req.session.user})
-        .then(newProperties =>{
-            const userid = req.session.user;
-            req.db.fetchParks({userid})
-            .then(parks =>{
-             const promises = parks.map(park=>{
-               const parkId= park.parkid
-              return parkDetail(parkId)
-             })
-            return Promise.all(promises)
-            })
-            .then(parksData =>{
-                res.send(parksData)
-            })
-            .catch(err=>{
-                throw err;
-            })
+    .catch(err => {
+      throw err;
+    });
+});
+app.delete(`/api/remove-comment/:id`, (req, res) => {
+  req.db.deleteComment(req.params.id).then(r => {
+    const parkid = req.body.park;
+    req.db
+      .getComments({ parkid })
+      .then(allComments => {
+        res.send(allComments);
+      })
+      .catch(err => {
+        throw err;
+      });
+  });
+});
+app.delete(`/api/remove/:id`, (req, res) => {
+  req.db
+    .removePark({ parkId: req.params.id, userId: req.session.user })
+    .then(newProperties => {
+      const userid = req.session.user;
+      req.db
+        .fetchParks({ userid })
+        .then(parks => {
+          const promises = parks.map(park => {
+            const parkId = park.parkid;
+            return parkDetail(parkId);
+          });
+          return Promise.all(promises);
         })
-    })
+        .then(parksData => {
+          res.send(parksData);
+        })
+        .catch(err => {
+          throw err;
+        });
+    });
+});
 function checkDb() {
-        return (req, res, next) => {
-            const db = app.get('db');
-                if (db) {
-                req.db = db;
-                next();
-                }
-        else {
-            res.status(500).send({ message: 'this died' });
-            }
-        };
-    };
-    app.use(express.static(path.join(__dirname, '/build')));
+  return (req, res, next) => {
+    const db = app.get("db");
+    if (db) {
+      req.db = db;
+      next();
+    } else {
+      res.status(500).send({ message: "this died" });
+    }
+  };
+}
 
-const port = process.env.PORT || 8080
-app.listen(port,()=>{
-  console.log(`Server listening on port ${port}`)
-} );
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
